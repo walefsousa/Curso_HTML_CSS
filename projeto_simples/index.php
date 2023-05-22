42376fbac58bc8acfd03e5498143c6df8dcbd985
Walef <walefsouza05@gmail.com>
2023-05-22 12:25:29 -0300

    Atualização do arquivo index.html

---
+++ projeto_simples/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Projeto_simples</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Nvidia</h1>
+        <p class="paragrafo1">NVIDIA ANUNCIA LANÇAMENTO DAS PLACAS DE VÍDEO GEFORCE RTX 4060 COM AVANÇOS TECNOLÓGICOS E SUPORTE AO DLSS</p>
+    </div>
+</body>
+</html>
